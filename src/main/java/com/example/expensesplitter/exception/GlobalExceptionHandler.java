c66684527df2cefd753767bdee877400d1075271
Rabindra Baisnab <i1000862@verisk.com>
2025-05-23 13:37:18 +0545

    feat: handle empty request body exception

--- src/main/java/com/example/expensesplitter/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/expensesplitter/exception/GlobalExceptionHandler.java
@@ -5,7 +5,7 @@ import com.example.expensesplitter.dto.response.ValidationErrorDetail;
 import com.example.expensesplitter.dto.response.ValidationErrorResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -20,37 +20,37 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponseDto> handleNotFound(ResourceNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(new ErrorResponseDto("NotFound", e.getMessage()));
+                             .body(new ErrorResponseDto("NotFound", e.getMessage()));
     }
 
     @ExceptionHandler(InvalidIdException.class)
     public ResponseEntity<ErrorResponseDto> handleInvalidUuid(InvalidIdException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(new ErrorResponseDto("BadRequest", e.getMessage()));
+                             .body(new ErrorResponseDto("BadRequest", e.getMessage()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ValidationErrorResponseDto> handleValidationException(MethodArgumentNotValidException e) {
-//        System.out.println("e.getMessage" + e.getMessage());
-//        System.out.println("e.getAllErrors" + e.getAllErrors());
-//        System.out.println("e.getBindingResult" + e.getBindingResult());
-//        System.out.println("e.getFieldErrors" + e.getFieldErrors());
-//        System.out.println("e.getFieldError" +e.getFieldError());
-
         List<ValidationErrorDetail> errorDetails = e.getBindingResult().getFieldErrors()
-                .stream()
-                .map(fieldError -> new ValidationErrorDetail(
-                        fieldError.getField(), fieldError.getDefaultMessage()
-                ))
-                .collect(Collectors.toList());
+                                                    .stream()
+                                                    .map(fieldError -> new ValidationErrorDetail(
+                                                            fieldError.getField(), fieldError.getDefaultMessage()
+                                                    ))
+                                                    .collect(Collectors.toList());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(new ValidationErrorResponseDto("BadRequest", "Validation failed.", errorDetails));
+                             .body(new ValidationErrorResponseDto("BadRequest", "Validation failed.", errorDetails));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponseDto> handleRequestBodyMissingException(HttpMessageNotReadableException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                             .body(new ErrorResponseDto("BadRequest", "Required request body is missing."));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDto> handleGenericException(Exception e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ErrorResponseDto("INTERNAL_SERVER_ERROR", e.getMessage()));
+                             .body(new ErrorResponseDto("INTERNAL_SERVER_ERROR", e.getMessage()));
     }
 }
